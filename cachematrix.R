68a7cf64d289bf37ea0e333fb1ac7dce3edd4ed3
Adrienne Breland <ab17744@yahoo.com>
2016-01-21 17:19:38 -0800

    Adrienne Breland commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+##These functions should be called in succession.
+##First, makeCacheMatrix is called  to create a matrix with 
+##additional functionaility in the form of a list
+##Second, cacheSolve will be called to retrieve the matrix inverse
+##either from cache, or via fresh calculation
+
+
+##This function takes a numeric vector and
+## a single dimension. 
+## It will create a list containing
+##   1) a square matrix
+##   2) a function to calucate the inverse and store the result in cache
+##   3) a function to retrieve the inverse from cache
+## example call:   makeCacheMatrix(matrix(c(2,3,4,5),nrow=2, ncol=2))
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  matInverse <<-NULL
+  inv <-NULL
+  calcInverse <-function(){ matInverse <<- solve(x)}
+  retrieveCache <-function () {matInverse}
+  list(retrieveCache=retrieveCache, calcInverse=calcInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function should be called with the matrix/list
+## created by makeCacheMatrix()
+## 
+## example call: cacheSolve( makeCacheMatrix(matrix(c(2,3,4,5),nrow=2, ncol=2)) )
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+          inv <- m$retrieveCache()
+	  if(!is.null(inv)) {
+	    ##message("getting cached data")
+	    inv
+	  } else {
+	    ##message("solving inverse")
+	    m$calcInverse()
+	    m$retrieveCache()
+  }
 }
